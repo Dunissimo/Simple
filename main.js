ea9bab96504a498cf18db5411452bf23833c949d
Dunissimo <SuperDen214@yandex.ru>
2024-03-17 03:45:28 +0400

    made burger, add animation

--- main.js
+++ main.js
@@ -7,10 +7,22 @@ import './styles/components.scss';
 const search = document.querySelector('.search');
 const searchOpen = search.querySelector('.open');
 const searchClose = search.querySelector('.close');
+const searchInput = search.querySelector('input');
 
 searchOpen.addEventListener('click', (e) => {
   search.classList.add('active');
 })
 searchClose.addEventListener('click', (e) => {
   search.classList.remove('active');
+  searchInput.value = '';
 })
+
+// Burger
+
+const burger = document.querySelector('.header__burger');
+const headerMenu = document.querySelector('.header__menu');
+
+burger.addEventListener('click', () => {
+  headerMenu.classList.toggle('active');
+  burger.classList.toggle('active');
+});
